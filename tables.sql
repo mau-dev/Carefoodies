f84b19af307d50f3926c29564795443fee765831
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-24 01:54:49 +0800

    initialised tables, should be updated for further user interaction

---
+++ tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username TEXT,
+	email TEXT,
+	password TEXT
+);
+
+CREATE TABLE posts (
+	id SERIAL PRIMARY KEY,
+	-- giver_id INTEGER,
+	-- receiver_id INTEGER,
+	-- pending_request BOOLEAN,
+	-- settled_request BOOLEAN,
+	title TEXT,
+	img TEXT,
+	ingredients TEXT,
+	area TEXT,
+	address TEXT,
+	content TEXT,
+	expiry_date DATE,
+	time_posted DATE
+
+);
