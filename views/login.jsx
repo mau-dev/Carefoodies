80c923dc06018560504c061295688769b829aba8
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-26 17:43:12 +0800

    added carousel to display additional tagline

--- views/login.jsx
+++ views/login.jsx
@@ -9,12 +9,32 @@ class Login extends React.Component {
             <div style={{fontFamily: 'Montserrat'}}>
                 <Navbar />
                 <main style={{backgroundColor: '#F7F7F5', height: '90vh'}}>
-                    <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '5% 0 0 10% ', float: 'left'}}>
-                        <h1 style={{color: '#CC46E0', margin: '6% auto ', width: '35%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share<br/> Excess<br/> Food<br/>
+                      <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '6% 0 0 6% ', float: 'left'}}>
+                       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
                         </h1>
-                        <p style={{paddingLeft: '32%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
-                    </div>
-                    <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        </h1>
+                        <p style={{paddingLeft: '30%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
+
+    </div>
+    <div class="carousel-item">
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        </h1>
+                        <p style={{paddingLeft: '30%', fontSize: '18px'}}>Waste less. Save money and help reducing food waste.</p>
+    </div>
+
+  </div>
+
+</div>
+                    </div>                    <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
                         <LoginForm /> {this.props.children}
                     </div>
                 </main>
--- views/register.jsx
+++ views/register.jsx
@@ -1,3 +1,35 @@
+// var React = require('react');
+// const Layout = require('./layouts');
+// const Navbar = require('./navbar');
+// const RegisterForm = require('./regFormComponent');
+
+// class Register extends React.Component {
+
+//   render() {
+//         return (
+//             <div style={{fontFamily: 'Montserrat'}}>
+//             <Navbar />
+//                 <main style={{backgroundColor: '#F7F7F5', height: '90vh'}}>
+//                      <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '6% 0 0 10% ', float: 'left'}}>
+//                         <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share
+//                         </h1>
+//                         <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
+//                         </h1>
+//                         <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+//                         </h1>
+//                         <p style={{paddingLeft: '30%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
+//                     </div>
+//                     <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
+//                         <RegisterForm /> {this.props.children}
+//                     </div>
+//                 </main>
+//             </div>
+//     );
+//   }
+// }
+
+// module.exports = Register;
+
 var React = require('react');
 const Layout = require('./layouts');
 const Navbar = require('./navbar');
@@ -10,9 +42,31 @@ class Register extends React.Component {
             <div style={{fontFamily: 'Montserrat'}}>
             <Navbar />
                 <main style={{backgroundColor: '#F7F7F5', height: '90vh'}}>
-                    <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '5% ', float: 'left'}}>
-                        <h1 style={{color: '#CC46E0', margin: '10% auto ', width: '35%', fontSize: '86px', fontWeight: '900'}}>Share<br/> Excess<br/> Food<br/>
+                     <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '6% 0 0 6% ', float: 'left'}}>
+                       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
                         </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        </h1>
+                        <p style={{paddingLeft: '30%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
+
+    </div>
+    <div class="carousel-item">
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        </h1>
+                        <p style={{paddingLeft: '30%', fontSize: '18px'}}>Waste less. Save money and help reducing food waste.</p>
+    </div>
+
+  </div>
+
+</div>
                     </div>
                     <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
                         <RegisterForm /> {this.props.children}
