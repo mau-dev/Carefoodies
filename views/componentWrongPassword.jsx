378bfbbfc9ace74f72cbbc429aacc66ac7b9c16c
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-29 20:06:11 +0800

    added pages to redirect to if wrong username or password

---
+++ views/componentWrongPassword.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+const Layout = require('./layouts');
+const Navbar = require('./navbar');
+
+
+class LogForm extends React.Component {
+
+  render() {
+
+        return (
+            <div style={{display: 'block', margin: '15% 3%'}}>
+
+                <form method="POST" action="/login" style={{width: '300px', display: 'block'}}>
+
+                    <input name="username" className="text-center" placeholder="Username" style={{width: '300px', textAlign: 'left', display: 'block', margin: '15px 0', height: '50px'}}/>
+
+                    <input name="email" className="text-center" placeholder="Email" style={{width: '300px', display: 'block', margin: '15px auto', height: '50px'}}/>
+                    <input name="password" className="text-center" placeholder="Password" style={{width: '300px', display: 'block', margin: '15px auto', height: '50px'}}/>
+                     <input type="submit" action="/posts" value= "Log In" className="btn btn-block" style={{width: '300px', display: 'block', margin: '10px auto', height: '50px', borderRadius: '8px', backgroundColor: '#CC46E0', color: 'white'}}/>
+                </form>
+                <p className="text-center" style={{color: 'red', width: '300px', margin: '0', padding: '0'}}>Invalid Password!</p>
+
+            </div>
+    );
+}
+}
+module.exports = LogForm;
---
+++ views/wrongUsername.jsx
@@ -0,0 +1,47 @@
+var React = require('react');
+const Layout = require('./layouts');
+const Navbar = require('./navbar');
+const WrongUser = require('./componentWrongUser');
+
+class Login extends React.Component {
+  render() {
+        return (
+            <div style={{fontFamily: 'Montserrat'}}>
+                <Navbar />
+                <main style={{backgroundColor: '#F7F7F5', height: '90vh'}}>
+                      <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '6% 0 0 6% ', float: 'left'}}>
+                       <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        </h1>
+                        <p style={{paddingLeft: '30%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
+
+    </div>
+    <div className="carousel-item">
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
+                        </h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        </h1>
+                        <p style={{paddingLeft: '30%', fontSize: '18px'}}>Waste less. Save money and help reducing food waste.</p>
+    </div>
+
+  </div>
+
+</div>
+                    </div>
+                                        <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
+                        <WrongUser/> {this.props.children}
+                    </div>
+                </main>
+            </div>
+    );
+  }
+}
+
+module.exports = Login;
