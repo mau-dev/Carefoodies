fb3caa49236d7efce65d7fc1866dc5fd029b4a09
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-30 12:56:35 +0800

    updated paths for heroku

--- views/navbar.jsx
+++ views/navbar.jsx
@@ -13,7 +13,7 @@ class Navbar extends React.Component {
                 <body>
                     <div className="container-fluid" style={{ padding:'0'}}>
                         <nav className="navbar navbar-expand-lg " style={{ padding: '10px 50px', borderBottom: '1px solid #E3E3E1', backgroundColor: 'white',fontFamily: 'Montserrat'}}>
-                            <span className="navbar-brand" href="./posts" style={{ color: '#CC46E0', fontWeight: 'bold'}}><img src="http://localhost:3000/images/carefoodies-logo.png" style={{ height: '30px', marginTop: '-13px'}}/></span>
+                            <span className="navbar-brand" style={{ color: '#CC46E0', fontWeight: 'bold'}}><a href="/"><img src="./images/carefoodies-logo.png" style={{ height: '30px', marginTop: '-13px'}}/></a></span>
                                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
--- views/welcome.jsx
+++ views/welcome.jsx
@@ -38,7 +38,7 @@ class Welcome extends React.Component {
                     </div>
                                         <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '0 5%', float: 'right'}}>
                                         <div style={{backgroundColor: '#ffffff', width: '80%', height: '70%', borderRadius: '8%', margin: '15% auto', padding: '40px'}}>
-                      <h3 style={{width: '70%', margin: '5% auto 0 auto', textAlign: 'center', fontFamily: 'Montserrat'}}> Thanks for the registration <span style={{color: '#5CE048'}}> {this.props.currentUser}</span>! </h3>
+                      <h3 style={{width: '70%', margin: '5% auto 0 auto', textAlign: 'center', fontFamily: 'Montserrat'}}> Welcome back <span style={{color: '#5CE048'}}> {this.props.currentUser}</span>! </h3>
                       <div style={{width: '90%', display: 'block', margin: '30px auto'}}>
                       <form className="d-inline my-2 my-lg-0" method="GET" action={"/posts"}>
                                     <input className="btn my-2 my-sm-0" type="submit" value="Start exploring" style={{ display: 'inline', marginLeft: '30px', border: '1px solid #CC46E0', color: '#CC46E0', borderRadius: '8px'}}/>
@@ -47,7 +47,7 @@ class Welcome extends React.Component {
                                     <input className="btn my-2 my-sm-0" type="submit" value="Share food" style={{ display: 'inline', marginLeft: '30px', backgroundColor: '#CC46E0', color: 'white', borderRadius: '8px'}}/>
                                 </form>
                                 </div>
-                                <img src="./images/welcome-img.png"  className="img-fluid" style={{float: 'left', width: '65%', height: 'auto', margin: '10% 20%'}}/>
+                                <img src="http://localhost:3000/images/welcome-img.png"  className="img-fluid" style={{float: 'left', width: '65%', height: 'auto', margin: '10% 20%'}}/>
                     </div>
                     </div>
                 </main>
