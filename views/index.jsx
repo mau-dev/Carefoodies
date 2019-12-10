a97046aaacdbc09ad7e43ea7ce981b79a6670bc3
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-12-10 22:40:32 +0800

    updated the grid to mobile and tablet responsive

--- views/index.jsx
+++ views/index.jsx
@@ -3,17 +3,17 @@ const Layout = require('./layouts');
 
 class Index extends React.Component {
 
-  render() {
+    render() {
         const postsList = this.props.posts.map(item => {
-        let { id, giver_id, posted_by, receiver_id, requested_by, pending_request, settled_request, title, img, ingredients, area, address, content, expiry_date, time_posted  } = item;
-         let button = <input className="btn my-2 my-sm-0" type="submit" value="Request" onClick="this.innerHTML = 'Pending...'" style={{ marginRight: '10px', float: 'right', backgroundColor: '#CC46E0', color: 'white', borderRadius: '8px'}}/>
+            let { id, giver_id, posted_by, receiver_id, requested_by, pending_request, settled_request, title, img, ingredients, area, address, content, expiry_date, time_posted } = item;
+            let button = <input className="btn my-2 my-sm-0" type="submit" value="Request" onClick="this.innerHTML = 'Pending...'" style={{ marginRight: '10px', float: 'right', backgroundColor: '#CC46E0', color: 'white', borderRadius: '8px'}}/>
 
-        if (pending_request === true ){
-            button = <input className="btn my-2 my-sm-0" type="submit" value="Pending" onClick="this.innerHTML = 'Pending...'" style={{ marginRight: '10px', float: 'right', backgroundColor: '#1A55D6', color: 'white', borderRadius: '8px'}}/>
-        }
-  return (
-            <div className="card col-md-3 float-left" style={{ margin: '50px 1%', maxWidth: '30%', borderRadius: '10px'}}>
-               <a href={"/posts/"+item.id}> <img height="300px" width="auto" src={item.img} href={"/posts/"+item.id} className="card-img-top" style={{  padding: '15px 0'}}/></a>
+            if (pending_request === true) {
+                button = <input className="btn my-2 my-sm-0" type="submit" value="Pending" onClick="this.innerHTML = 'Pending...'" style={{ marginRight: '10px', float: 'right', backgroundColor: '#1A55D6', color: 'white', borderRadius: '8px'}}/>
+            }
+            return (
+                <div className="card col-xl-3 col-lg-3 col-md-5 col-sm-9 col-10 float-left" style={{ margin: '50px 1%', borderRadius: '10px'}}>
+               <a href={"/posts/"+item.id}> <img height="300px" width="auto" src={item.img} href={"/posts/"+item.id} className="card-img-top" style={{  padding: '15px 0', objectFit: 'cover'}}/></a>
                 <div className="card-bodys" style={{  marginBottom: '20px'}}>
                      <h5 className="card-title">{title}</h5>
                     <p className="card-text">{address}</p>
@@ -30,20 +30,22 @@ class Index extends React.Component {
                 </div>
             </div>
 
-          )
-});
+            )
+        });
 
 
-return (
+        return (
+            <div>
             <Layout>
                 <div className="container-fluid d-flex justify-content-center" style={{ backgroundColor: '#F7F7F5'}}>
-                    <div className="row d-flex flex-row flex-wrap align-items-center justify-content-around" >
+                    <div className="row d-flex flex-row flex-wrap align-items-center justify-content-center" >
                     {postsList}
                     </div>
                 </div>
              </Layout>
-    );
-}
+             </div>
+        );
+    }
 }
 
 module.exports = Index;
