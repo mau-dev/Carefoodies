e5eeb5a0d7f77965dc515ad8e899296b176f9a85
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-12-31 12:04:00 +0800

    bootstrap mobile on chrome not working

--- views/layouts.jsx
+++ views/layouts.jsx
@@ -7,6 +7,7 @@ class Layout extends React.Component {
         return (
             <html>
                 <head>
+                <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"></link>
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans|Roboto:900&display=swap" rel="stylesheet"></link>
@@ -15,7 +16,7 @@ class Layout extends React.Component {
 
 
                 </head>
-                <body>
+                <body style={{backgroundColor: '#F7F7F5'}}>
                     <div className="container-fluid" style={{ padding:'0'}}>
                     <NavbarLogged />
                         <main style={{  margin: '0 auto', fontFamily: 'Montserrat'}}>
--- views/register.jsx
+++ views/register.jsx
@@ -42,24 +42,26 @@ class Register extends React.Component {
             <div style={{fontFamily: 'Montserrat'}}>
             <Navbar />
                 <main style={{backgroundColor: '#F7F7F5', height: '90vh'}}>
-                     <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '6% 0 0 6% ', float: 'left'}}>
+                <div className="container">
+                <div classname="row">
+                     <div className="col-md-6 col-sm-12 col-12" style={{backgroundColor: '#F7F7F5', height: '100%', display: 'block', margin: '0',  float: 'left'}}>
                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ',  fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
                         </h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ',  fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
                         </h1>
                         <p style={{paddingLeft: '30%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
 
     </div>
     <div className="carousel-item">
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ',  fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
                         </h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
                         </h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ',fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
                         </h1>
                         <p style={{paddingLeft: '30%', fontSize: '18px'}}>Waste less. Save money and help reducing food waste.</p>
     </div>
@@ -68,8 +70,10 @@ class Register extends React.Component {
 
 </div>
                     </div>
-                    <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
+                    <div className=" col-md-6 col-sm-12 col-12" style={{backgroundColor: '#F7F7F5', height: '100%', display: 'block', margin: '0 ',  float: 'right'}}>
                         <RegisterForm /> {this.props.children}
+                    </div>
+                    </div>
                     </div>
                 </main>
             </div>
