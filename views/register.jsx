f990694fd986530c20a47f11ed8f135930034a6e
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-06 09:53:52 +0800

    register page back to previous version

--- views/register.jsx
+++ views/register.jsx
@@ -40,31 +40,29 @@ class Register extends React.Component {
 
   render() {
         return (
-            <div>
+           <div>
             <Head />
             <div style={{fontFamily: 'Montserrat'}}>
-            <Navbar />
+                <Navbar />
                 <main style={{backgroundColor: '#F7F7F5', minHeight: '100vh'}}>
-                <div className="container reg-container d-flex">
-                <div className="row d-flex flex-wrap justify-content-around">
-                     <div className="float-left  d-flex reg-left col-md-6 col-sm-10 col-11" style={{backgroundColor: '#F7F7F5', height: '100%', display: 'block', margin: '0',  float: 'left'}}>
+                      <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0', padding: '6% 0 0 6% ', float: 'left'}}>
                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ',  fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Share</h1>
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Excess
                         </h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ',  fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
                         </h1>
                         <p style={{paddingLeft: '30%', fontSize: '18px'}}>Share the food you don’t need instead of throwing it away and help other people while helping the environment</p>
 
     </div>
     <div className="carousel-item">
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ',  fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Find
                         </h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto ', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
+                        <h1 style={{color: '#CC46E0', margin: '0 auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Free
                         </h1>
-                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ',fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
+                        <h1 style={{color: '#CC46E0', margin: '0 auto 2% auto ', width: '40%', fontSize: '86px', fontWeight: '900', borderBottom: '25px solid #5CE048'}}>Food
                         </h1>
                         <p style={{paddingLeft: '30%', fontSize: '18px'}}>Waste less. Save money and help reducing food waste.</p>
     </div>
@@ -73,10 +71,8 @@ class Register extends React.Component {
 
 </div>
                     </div>
-                    <div className=" form-group reg-form  d-flex col-md-6 col-sm-10 col-11 " style={{backgroundColor: '#F7F7F5', height: '100%', display: 'block', margin: '0 ',  float: 'right'}}>
+                                        <div style={{backgroundColor: '#F7F7F5', width: '50vw', height: '100%', display: 'block', margin: '0 ', padding: '10% ', float: 'right'}}>
                         <RegisterForm /> {this.props.children}
-                    </div>
-                    </div>
                     </div>
                 </main>
             </div>
