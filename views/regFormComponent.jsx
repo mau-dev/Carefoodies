1839c8835143882e8f1443e5c1e0cfa1e72aa657
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-06 14:06:36 +0800

    updating styles to responsive on register page

--- views/regFormComponent.jsx
+++ views/regFormComponent.jsx
@@ -9,7 +9,7 @@ class RegForm extends React.Component {
 
         return (
 
-            <div style={{display: 'block', margin: '15% 3%'}}>
+            <div style={{display: 'flex', alignItems: "center"}}>
                 <form method="POST" action="/register" style={{width: '300px', display: 'block'}}>
                     <input name="username" className="text-center" placeholder="Username" style={{width: '300px', textAlign: 'left', display: 'block', margin: '15px 0', height: '50px'}}/>
                     <input name="email" className="text-center" placeholder="Email" style={{width: '300px', display: 'block', margin: '15px auto', height: '50px'}}/>
