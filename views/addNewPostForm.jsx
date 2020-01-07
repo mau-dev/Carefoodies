f4158bfcdc5c42042fafa86e2a8b837382a2e920
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-07 12:19:17 +0800

    add new post form, mobile responsive

--- views/addNewPostForm.jsx
+++ views/addNewPostForm.jsx
@@ -9,18 +9,21 @@ class NewPost extends React.Component {
         <div>
         <Head />
            <NavbarLogged />
-                <div style={{width: '100%', height: '90vh',backgroundColor: '#F7F7F5'}}>
-                    <form method="post" action="/posts" style={{width: '500px', margin: '0 auto'}} ><br/><br/>
-                        <h3 className="text-center" style={{width: '500px', margin: '0 auto'}}>Post food: </h3><br/>
-                        <input type="text" name="title" placeholder="Title" style={{width: '500px', height: '50px'}} /><br/><br/>
-                        <input type="text" name="ingredients" placeholder="Ingredients" style={{width: '500px', height: '50px'}} /><br/><br/>
-                        <input type="url" name="img" placeholder="Upload Img"  style={{width: '500px'}}/><br/><br/>
-                        <input type="text" name="area" placeholder="District/area" style={{width: '500px', height: '50px'}} /><br/><br/>
-                        <input type="text" name="address" placeholder="Address" style={{width: '500px', height: '50px'}} /><br/><br/>
-                        <input type="text" name="content" placeholder="Meal Info" style={{width: '500px', height: '50px'}} /><br/><br/>
-                        <input type="text" name="expiry_date" placeholder="Expiry Date" style={{width: '500px', height: '50px'}} /><br/><br/>
-                        <input type="submit" value="Submit" className="btn  btn-block" style={{width: '500px', backgroundColor: '#CC46E0', color: 'white'}} />
+                <div style={{width: '100%', monHeight: '100vh',backgroundColor: '#F7F7F5'}}>
+                <div className="container col-md-10" style={{ margin: '0 auto'}}>
+                    <form method="post" action="/posts" className="col-md-8 col-11 text-center" style={{ margin: '0 auto', paddingBottom: '2rem'}} ><br/><br/>
+                        <h3 className="text-center" className="col-md-8 col-11" style={{margin: '0 auto'}}>Post food: </h3><br/>
+                        <input type="text" name="title" placeholder="Title" className="col-md-8 col-11" style={{ height: '50px'}} /><br/><br/>
+                        <input type="text" name="ingredients" placeholder="Ingredients" className="col-md-8 col-11" style={{ height: '50px'}} /><br/><br/>
+                        <input type="url" name="img" placeholder="Upload Img"  className="col-md-8 col-11"/><br/><br/>
+                        <input type="text" name="area" placeholder="District/area"
+                        className="col-md-8 col-11"style={{ height: '50px'}} /><br/><br/>
+                        <input type="text" name="address" placeholder="Address" className="col-md-8 col-11" style={{ height: '50px'}} /><br/><br/>
+                        <input type="text" name="content" placeholder="Meal Info" className="col-md-8 col-11" style={{height: '50px'}} /><br/><br/>
+                        <input type="text" name="expiry_date" placeholder="Expiry Date"  className="col-md-8 col-11"style={{ height: '50px'}} /><br/><br/>
+                        <input type="submit" value="Submit" className="btn  btn-block"  className="col-md-8 col-11"style={{ backgroundColor: '#CC46E0', height: '50px', color: 'white', border: 'none', borderRadius: '8px'}} />
                     </form>
+                    </div>
                 </div>
 
         </div>
